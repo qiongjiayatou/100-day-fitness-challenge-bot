32247fd58ccbf3f4f28126e9d375bfcc3e3db6dd
qiongjiayatou <qiongjiayatou@gmail.com>
2024-09-09 22:07:46 +0300

    upd: Debug deploy

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 # Database configuration
-POSTGRES_DB = os.environ.get("POSTGRES_DB")
-POSTGRES_USER = os.environ.get("POSTGRES_USER")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "db")
-POSTGRES_PORT = os.environ.get("POSTGRES_PORT", "5432")
+POSTGRES_DB=os.environ.get("POSTGRES_DB")
+POSTGRES_USER=os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD=os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_HOST=os.environ.get("POSTGRES_HOST", "db")
+POSTGRES_PORT=os.environ.get("POSTGRES_PORT", "5432")
 
 # Other configurations
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
